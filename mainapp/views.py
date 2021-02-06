c94316559744f81386e691a828ca69a95c4441ec
Al1Gol <78210092+Al1Gol@users.noreply.github.com>
2021-02-06 13:30:37 +0300

    Merge to lession_2
    * Lession_2
    
    * Lesson 2. Context for templates
    
    * Lesson 2. Repeat and filters
    
    * correct files for pep-8
    
    Co-authored-by: ikidzuki <ikidzuki@mail.ru>

--- mainapp/views.py
+++ mainapp/views.py
@@ -1,12 +1,85 @@
+import datetime
+
 from django.shortcuts import render
 
-# Create your views here.
 
 def main(request):
-    return render(request, "mainapp/index.html")
+    title = "главная"
+    products = [
+        {
+            "name": "Отличный стул",
+            "desc": "Расположитесь комфортно.",
+            "img_src": "product-1.jpg",
+            "img_href": "/product/1/",
+            "alt": "продукт 1",
+        },
+        {
+            "name": "Стул повышенного качества",
+            "desc": "Не оторваться.",
+            "img_src": "product-2.jpg",
+            "img_href": "/product/2/",
+            "alt": "продукт 2",
+        },
+    ]
+    content = {"title": title, "products": products}
+    return render(request, "mainapp/index.html", content)
+
 
 def products(request):
-    return render(request, "mainapp/products.html")
+    title = "Продукты"
+    links_menu = [
+        {"href": "products_all", "name": "все"},
+        {"href": "products_home", "name": "дом"},
+        {"href": "products_office", "name": "офис"},
+        {"href": "products_modern", "name": "модерн"},
+        {"href": "products_classic", "name": "классика"},
+    ]
+    same_products = [
+        {
+            "name": "Отличный стул", 
+            "desc": "Не оторваться.", 
+            "img_src": "product-11.jpg", 
+            "alt": "продукт 11"
+        },
+        {
+            "name": "Стул повышенного качества", 
+            "desc": "Комфортно.", 
+            "img_src": "product-21.jpg", 
+            "alt": "продукт 21"
+        },
+        {
+            "name": "Стул премиального качества",
+            "desc": "Просто попробуйте.",
+            "img_src": "product-31.jpg",
+            "alt": "продукт 31",
+        },
+    ]
+    content = {"title": title, "links_menu": links_menu, "same_products": same_products}
+    return render(request, "mainapp/products.html", content)
+
 
 def contact(request):
-    return render(request, "mainapp/contact.html")
+    title = "О нас"
+    visit_date = datetime.datetime.now()
+    locations = [
+        {
+            "city": "Москва", 
+            "phone": "+7-888-888-8888", 
+            "email": "info@geekshop.ru", 
+            "address": "В пределах МКАД"
+        },
+        {
+            "city": "Екатеринбург",
+            "phone": "+7-777-777-7777",
+            "email": "info_yekaterinburg@geekshop.ru",
+            "address": "Близко к центру",
+        },
+        {
+            "city": "Владивосток",
+            "phone": "+7-999-999-9999",
+            "email": "info_vladivostok@geekshop.ru",
+            "address": "Близко к океану",
+        },
+    ]
+    content = {"title": title, "visit_date": visit_date, "locations": locations}
+    return render(request, "mainapp/contact.html", content)
