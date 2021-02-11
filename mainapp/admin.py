1545f58476f0a33c35dd76f9f70e96108f99c7b3
ikidzuki <ikidzuki@mail.ru>
2021-02-11 05:46:14 +0300

    Lesson 4. Auth app and user model

--- mainapp/admin.py
+++ mainapp/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from .models import Products, ProductCategory
+from .models import Contact, Products, ProductCategory
 
 # Registefrom django.contrib import admin
 
 admin.site.register(ProductCategory)
 admin.site.register(Products)
+admin.site.register(Contact)
